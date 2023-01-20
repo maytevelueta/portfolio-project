ddc8e5f2a5b0071aab5f3c22982a8ce06c63f3a9
Mayte Velueta <maytevelueta@gmail.com>
2023-01-20 16:17:54 -0600

    2001

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -7,53 +7,54 @@ const Result = () => {
   );
 };
 
-function ContactForm() {
+/*function ContactForm() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  
+  const [message, setMessage] = useState('');*/
+  function ContactForm() {
   const form = useState();
   const [result, showResult] = useState(false);
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_mp95mmb', 'template_lkf72o6', form.current, 'W4hMrFL5c8e7cdZxY')
-      .then((result) => {
+    emailjs.sendForm('service_mp95mmb', 'template_lkf72o6', e.target, 'W4hMrFL5c8e7cdZxY')
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+      }, 
+      (error) => {
           console.log(error.text);
-      });
+      }
+      );
       e.target.reset();
       showResult(true);
   };
-
+ 
   setTimeout(() => {
-    ContactForm(false);
-  }, 5000);
+    result(false);
+  }, 10000);
 
   return (
-    <div>
-      <form ref={form} onSubmit={sendEmail}>
+      <form ref={form} action="" onSubmit={sendEmail}>
         <div className="form-group">
             <label htmlFor='name'>Your name
-            <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+            <input type="text" id="name" name="name" /*value={name} onChange={(e) => setName(e.target.value)}*/ />
             </label>
         </div>
         <div className="form-group">
             <label htmlFor='email'>Your email
-            <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="text" id="email" name="email" /*value={email} onChange={(e) => setEmail(e.target.value)}*/ />
             </label>
         </div>
         <div className="form-group">
             <label htmlFor='message'>Your message
-            <textarea type="text" id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+            <textarea type="text" id="message" name="message" /*value={message} onChange={(e) => setMessage(e.target.value)}*/ />
             </label>
         </div>
         <button type='submit'>Send</button>
         <div className='sent'>{result ? <Result /> : null}</div>
       </form>
-    </div>
   );
-};
+  };
 
 export default ContactForm;
