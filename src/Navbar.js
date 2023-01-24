eb0cca3d9a12fd07ab9f1f67e22bc20df6e5ef4a
Mayte Velueta <maytevelueta@gmail.com>
2023-01-23 18:49:33 -0600

    2301

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import {Link} from "react-scroll";
+import {FaBars, FaTimes} from 'react-icons/fa';
 import logo from "./assets/logo.png";
 
 const Navbar = () => {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
     const [color, setColor] = useState(false)
     const changeColor = () => {
         if (window.scrollY >= 5) {
@@ -20,11 +25,21 @@ const Navbar = () => {
             <a href="/" className="logo">
               <img src={logo} alt="logo"/>
             </a>
-            <div className="links">
-                <a href="/">Home</a>
-                <Link to="project-list" spy={true} smooth={true} offset={-100} duration={500} style={{cursor:"pointer"}}>Projects</Link>
-                <Link to="contact" spy={true} smooth={true} offset={-100} duration={500} style={{cursor:"pointer"}}>Contact</Link>
+            <div className="hamburger" onClick={handleClick}>
+                { click ? (<FaTimes size={30} style={{color: '#ffffff'}} />) :
+                (<FaBars size={30} style={{color: '#85586F'}} />)}
             </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+                <li className="nav-link">
+                <a href="/">Home</a>
+                </li>
+                <li className="nav-link">
+                <Link to="project-list" spy={true} smooth={true} offset={-100} duration={500} style={{cursor:"pointer"}}>Projects</Link>
+                </li>
+                <li className="nav-link">
+                <Link to="contact" spy={true} smooth={true} offset={-100} duration={500} style={{cursor:"pointer"}}>Contact</Link>
+                </li>
+            </ul>
         </nav>
         </div>
      );
