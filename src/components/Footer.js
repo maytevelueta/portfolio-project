f934c1b90f4208a49513772d0974a36e20b78fe5
Mayte Velueta <maytevelueta@gmail.com>
2023-01-23 22:17:39 -0600

    2301

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer ({
         <footer className="footer">
             <div className="footer-links">
             <a href="https://github.com/maytevelueta" style={{ fill: "#AC7D88", fontSize: "1.5em" }}>{GitHub}</a>
-            <a href="https://www.linkedin.com/in/mayté-rodríguez-velueta-66a295a1/" style={{ fill: "#AC7D88", fontSize: "1.5em" }}>{LinkedIn}</a>
+            <a href="https://www.linkedin.com/in/mayte-velueta" style={{ fill: "#AC7D88", fontSize: "1.5em" }}>{LinkedIn}</a>
             </div>
         </footer>
      );
