f9103ea6273eaaff8ffd373de4c30484984a6fa7
Mayte Velueta <maytevelueta@gmail.com>
2024-01-22 16:23:53 +0000

    2201

--- src/components/Values.js
+++ src/components/Values.js
@@ -2,7 +2,7 @@ const Values = ({values, title}) => {
     return ( 
         <div>
         <h2 style={{ marginBottom: "2rem" }}>{ title }</h2>
-        <div className="values-list">
+        <div className="values-container">
             {values.map((value) => (
                 <div className="value-preview" key={value.id}>
                     <h2 className="values-text" style={{ color: "#85586F" }}>{ value.title }</h2>
